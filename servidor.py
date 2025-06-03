6561121d053404bbdbb3373c0933e7ab84ddee1a
AntonioLaureno28 <antoniopedro2802@gmail.com>
2025-06-03 20:16:41 -0300

    Atualizando o servidor

--- servidor.py
+++ servidor.py
@@ -1,4 +1,77 @@
 from socket import *
+import threading
+import time
+
+jogadores = []
+jogadas = {}
+lock = threading.Lock()
+
+def analise_vencedor(lance_1, lance_2):
+    resultado = ""
+    if lance_1 == lance_2:
+        resultado = "Empate"
+    elif (lance_1 == "pedra" and lance_2 == "tesoura") or \
+     (lance_1 == "tesoura" and lance_2 == "papel") or \
+     (lance_1 == "papel" and lance_2 == "pedra"):
+    
+        resultado = "Jogador 1 Venceu"
+    else:
+        resultado = "Jogador 2 Venceu"
+    return resultado
+
+
+def gerenciar_jogadas(conexao, endereco):
+
+    global jogadores, jogadas
+
+    with lock:
+        #Verifica se os 2 jogadores estão ocupando a rodada, se tiverem, a conexão é fechada
+        if len(jogadores) >= 2:
+            conexao.sendall("Jogo lotado.".encode('utf-8'))
+            conexao.close()
+            return
+        else:
+            jogadores.append(conexao)
+            
+    try:
+
+        #Verifica se temos jogadores suficientes para começar a rodada
+        if len(jogadores) == 1:
+            conexao.sendall("Aguardando outro oponente".encode('utf-8'))
+            while len(jogadores) < 2:
+                time.sleep(2)
+        conexao.sendall("O jogo vai começar".encode('utf-8'))
+
+
+        jogada = conexao.recv(1024).decode('utf-8')
+        with lock:
+            jogadas[conexao] = jogada
+            
+            if len(jogadas) == 2:
+                j1_conn, j2_conn = jogadores[0], jogadores[1]
+                lance_j1 = jogadas[j1_conn]
+                lance_j2 = jogadas[j2_conn]
+                result = analise_vencedor(lance_j1, lance_j2)
+
+                if result == "Jogador 1 Venceu":
+                    j1_conn.sendall(f"Você Venceu! Você: {lance_j1} ; Oponente: {lance_j2}".encode('utf-8'))
+                    j2_conn.sendall(f"Você Perdeu! Você: {lance_j2} ; Oponente: {lance_j1}".encode('utf-8'))
+                elif result == "Jogador 2 Venceu":
+                    j2_conn.sendall(f"Você Venceu! Você: {lance_j2} ; Oponente: {lance_j1}".encode('utf-8'))
+                    j1_conn.sendall(f"Você Perdeu! Você: {lance_j1} ; Oponente: {lance_j2}".encode('utf-8'))
+                else:
+                    j1_conn.sendall(f"Empate! Você: {lance_j1} ; Oponente: {lance_j2}".encode('utf-8'))
+                    j2_conn.sendall(f"Empate! Você: {lance_j2} ; Oponente: {lance_j1}".encode('utf-8'))
+
+                jogadores.clear()
+                jogadas.clear()
+
+    except Exception as e:
+        print("Ocorreu um erro ao receber a jogada")
+    finally:
+        print("Operação encerrada.")
+        conexao.close()
+
 
 host = gethostname()
 port = 55551
@@ -7,39 +80,11 @@ print(host, port)
 
 serv = socket(AF_INET, SOCK_STREAM)
 serv.bind((host, port))
-serv.listen(1)
+serv.listen(2)
 
 
-print(f"Servidor iniciado em {host}:{port}. Esperando por um oponente.")
-
-conn, addr = serv.accept()
-print(f'Oponente conectado em {addr}')
-
-jogada_1 = ""
-while jogada_1 not in ["pedra", "papel", "tesoura"]:
-    jogada_1 = input("Escolha sua jogada, jogador 1")
-
-jogada_2 = conn.recv(1024).decode('utf-8') #Recebe as informações do cliente
-
-print(f'jogador 2 escolheu: {jogada_2}')
-
-resultado = ""
-if jogada_1 == jogada_2:
-    resultado = "Empate"
-elif (jogada_1 == "pedra" and jogada_2 == "tesoura") or \
-     (jogada_1 == "tesoura" and jogada_2 == "papel") or \
-     (jogada_1 == "papel" and jogada_2 == "pedra"):
-    
-    resultado = "Jogador 1 Venceu"
-else:
-    resultado = "Jogador 2 Venceu"
-
-print(resultado)
-conn.sendall(resultado.encode('utf-8')) #Envia o resultado para o cliente
-
-conn.close()
-serv.close()
-print("Conexão fechada.")
-
-
+while True:
+    conexao, endereco = serv.accept()
+    thread = threading.Thread(target=gerenciar_jogadas, args=(conexao, endereco))
+    thread.start()
 
